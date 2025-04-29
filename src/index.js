dd234ce43608f0f48ac73bcffe9e7e69e52b7e06
Saief <A.S.Bokor@outlook.com>
2025-04-29 23:57:59 +0100

    add prettier to the template repo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./styles.css";
 
-
 console.log("Hello, World!");
 
-const unusedVariable = 42
+const unusedVariable = 42;
 
 console.log("welcome to the To-Do List App!");
+
+function tabTest() {
+    console.log("tab changed");
+}
